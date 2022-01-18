f1401c10351e32b128026d9b3d169316c40586d3
folzieds <folzieds@gmail.com>
2022-01-18 23:08:22 +0100

    Project setup

---
+++ src/test/java/com/phos/shin/ShinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phos.shin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
